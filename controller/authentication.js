9a420eff08930d4b809cad3222cc3aef80484bec
Khang Nguyen <nguyenkhang1400@gmail.com>
2025-03-08 21:17:12 +0700

    update 2 token

--- controller/authentication.js
+++ controller/authentication.js
@@ -1,31 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const userModel = require("../model/user");
+const { generateAccessToken, generateRefreshToken } = require("../utility/jwt");
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await userModel.findOne({ phone: username });
   if (!user) {
     return res.status(404).json({ message: "userNotFound" });
   }
-
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
     return res.status(400).json({ message: "passwordIsIncorrect" });
   }
-
-  const accessToken = jwt.sign(
-    { id: user._id, username: user.username },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
-
   res.json({
     message: "loginSuccess",
     data: {
-      accessToken: accessToken,
-      refreshToken: "",
+      accessToken: generateAccessToken(user),
+      refreshToken: generateRefreshToken(user),
     },
   });
 });
---
+++ utility/jwt.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user._id, username: user.username, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(
+    { id: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+}
+
+module.exports = {
+    generateAccessToken,
+    generateRefreshToken
+}
